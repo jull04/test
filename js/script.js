b397604d9f2a95e7981d3d43d2a1e6150e8ab1ff
Julia Khabibova <julia.khabibova@mail.ru>
2025-10-15 22:16:34 +0300

    range persentage add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
+const rangeInput = document.querySelector('.form__range');
+const rangeValue = document.querySelector('.form__range-value');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   nav.classList.toggle('header__nav--active');
   document.body.classList.toggle('no-scroll'); 
 });
+
+if (rangeInput && rangeValue) {
+  rangeInput.addEventListener('input', () => {
+    rangeValue.textContent = `${rangeInput.value}%`;
+  });
+}
